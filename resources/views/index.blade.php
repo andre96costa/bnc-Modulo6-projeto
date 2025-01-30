e4765c97d5d5f61913b65599da563526dd6a30b6
Andre Costa <andre2396costa@gmail.com>
2025-01-29 21:18:52 -0300

    bread-crumb

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,7 @@
 <x-layouts.base>
-
+    <div class="container py-12 flex flex-col">
+        <h1 class="text-title-5 font-bold ">Product Name</h1>
+        <x-bread-crumb :items="['Home','Product','Product Name']"></x-bread-crumb>
+        
+    </div>
 </x-layouts.base>
