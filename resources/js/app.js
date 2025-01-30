11790b7cd94fb1f003bd4cc2ff6aec5baeaef248
Andre Costa <andre2396costa@gmail.com>
2025-01-30 20:51:43 -0300

    dinamic carousel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 import Alpine from 'alpinejs'
- 
+import carousel from "./carousel";
+
 window.Alpine = Alpine
- 
+Alpine.data('carousel', carousel)
 Alpine.start()
--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -1,16 +1,19 @@
-<div class="flex flex-col w-full">
+<div 
+    class="flex flex-col w-full"
+    x-data="carousel(@js($images))"
+>
     <div class="p-2 overflow-hidden border border-solid border-gray-300 rounded mt-4">
-        <img src="{{ Vite::asset("resources/images/$images[0]") }}" alt="image" class="w-full object-scale-down">
+        <img x-ref="highlight" src="{{ Vite::asset("resources/images/$images[0]") }}" alt="image" class="w-full object-scale-down">
     </div>
 
     <div class="relative flex flex-row items-center justify-center space-x-2 mt-2">
 
-        <x-icon name="chevron_left" size="large" class="cursor-pointer absolute left-0"></x-icon>
+        <x-icon @click="changeImage('previous')" name="chevron_left" size="large" class="cursor-pointer absolute left-0"></x-icon>
 
         @foreach ($images as $image)
-            <img src="{{ Vite::asset("resources/images/$image") }}" alt="image" class="w-12 h-12 object-cover object-center rounded-md bg-gray-300 cursor-pointer hover:bg-gray-400">            
+            <img x-ref="image{{$loop->index}}" @click="$refs.highlight.src = $event.target.src" src="{{ Vite::asset("resources/images/$image") }}" alt="image" class="w-12 h-12 object-cover object-center rounded-md bg-gray-300 cursor-pointer hover:bg-gray-400">            
         @endforeach
 
-        <x-icon name="chevron_right" size="large" class="cursor-pointer absolute right-0"></x-icon>
+        <x-icon @click="changeImage('next')" name="chevron_right" size="large" class="cursor-pointer absolute right-0"></x-icon>
     </div>
 </div>
